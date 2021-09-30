8b2888b7bb429eaa467318cf164057ad3e4c4f40
Arham Saqib <arhamsaqib@outlook.com>
2021-10-01 01:44:26 +0500

    account main

--- app/screens/patient/account/account.tsx
+++ app/screens/patient/account/account.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import {Text, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import {COLORS} from '../../../colors';
+import {GlobalStyles} from '../../../styles/globalStyles';
+
+interface CProps {
+  onPress?(): void;
+  name?: string;
+}
+
+const Card = (props: CProps) => {
+  return (
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={[styles.card, GlobalStyles.elevated_card, {marginVertical: 5}]}>
+      <Text style={styles.head}>{props.name}</Text>
+    </TouchableOpacity>
+  );
+};
 
 export const Account = ({navigation}: any) => {
   return (
     <SafeAreaView style={styles.main}>
-      <Text>Account</Text>
+      <View style={{width: '90%', marginBottom: 20}}>
+        <Text style={styles.title}>Account</Text>
+      </View>
+
+      <View style={{width: '90%'}}>
+        <Card name="Profile" />
+        <Card name="Change Password" />
+      </View>
     </SafeAreaView>
   );
 };
@@ -13,5 +43,21 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  title: {
+    letterSpacing: -1,
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  head: {
+    letterSpacing: -1,
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: COLORS.dark_blue,
+  },
+  card: {
+    padding: 10,
+    justifyContent: 'center',
   },
 });
