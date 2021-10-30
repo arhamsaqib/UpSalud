cf227831e4012c964694bb25a0560919347feac2
Arham Saqib <arhamsaqib@outlook.com>
2021-10-30 18:33:57 +0500

    User profile hooked

--- app/screens/patient/account/profile.tsx
+++ app/screens/patient/account/profile.tsx
@@ -1,11 +1,68 @@
 import React from 'react';
-import {Text, SafeAreaView, StyleSheet, View} from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
 import {ButtonStandard} from '../../../core/button';
 import {MyText} from '../../../core/text';
 import {TextInputStandard} from '../../../core/textInput';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {useState} from 'react';
+import {RootStateOrAny, useSelector} from 'react-redux';
+import {
+  showUserBasicInformation,
+  storeBasicInformation,
+} from '../../../api/basicInformation';
+import {useEffect} from 'react';
+import {COLORS} from '../../../colors';
+import auth from '@react-native-firebase/auth';
+import {showUser} from '../../../api/users';
 
 export const Profile = ({navigation}: any) => {
+  const [loader, setLoader] = useState(false);
+  const [data, setData]: any = useState([]);
+  const [user, setUser]: any = useState([]);
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [idnumber, setIdnumber] = useState('');
+  const [dob, setDob] = useState('');
+  const state = useSelector((state: RootStateOrAny) => state.CurrentUser);
+  async function onSavePress() {
+    setLoader(true);
+    const data = {
+      fname: fname,
+      lname: lname,
+      uid: state.id.toString(),
+      dob: dob,
+      id_number: idnumber,
+    };
+    const res = await storeBasicInformation(data).finally(() => {
+      setLoader(false);
+    });
+    //console.log(res, 'Basic information store response');
+    Alert.alert('Updated!');
+  }
+  async function FetchAPI() {
+    const uid: any = auth().currentUser?.uid;
+    const userI = await showUser(uid);
+    if (userI.fuid !== undefined) setUser(userI);
+
+    setLoader(true);
+    const res = await showUserBasicInformation(state.id).finally(() => {
+      setLoader(false);
+    });
+    if (res.uid !== undefined) {
+      setData(res);
+    }
+    //console.log(res, 'User Basic info fetched');
+  }
+  useEffect(() => {
+    FetchAPI();
+  }, []);
   return (
     <SafeAreaView style={styles.main}>
       <View
@@ -21,24 +78,44 @@ export const Profile = ({navigation}: any) => {
           onPress={() => navigation.goBack()}
         />
         <Text style={styles.title}>Profile</Text>
+        {loader && (
+          <ActivityIndicator
+            style={{marginLeft: 10}}
+            color={COLORS.dark_blue}
+          />
+        )}
       </View>
 
       <View style={{width: '90%'}}>
         <View style={styles.infoCont}>
           <MyText style={styles.head}>Email</MyText>
-          <TextInputStandard />
+          <TextInputStandard defaultValue={user.email} editable={false} />
           <MyText style={styles.head}>First Name</MyText>
-          <TextInputStandard />
+          <TextInputStandard
+            defaultValue={data.fname}
+            onChangeText={setFname}
+          />
           <MyText style={styles.head}>Last Name</MyText>
-          <TextInputStandard />
+          <TextInputStandard
+            defaultValue={data.lname}
+            onChangeText={setLname}
+          />
           <MyText style={styles.head}>ID Number</MyText>
-          <TextInputStandard />
+          <TextInputStandard
+            defaultValue={data.id_number}
+            onChangeText={setIdnumber}
+          />
           <MyText style={styles.head}>Date of Birth</MyText>
-          <TextInputStandard />
+          <TextInputStandard defaultValue={data.dob} onChangeText={setDob} />
         </View>
       </View>
       <View style={styles.bottom}>
-        <ButtonStandard title="Save" />
+        <ButtonStandard
+          title="Save"
+          onPress={onSavePress}
+          loading={loader}
+          disabled={loader}
+        />
       </View>
     </SafeAreaView>
   );
@@ -70,6 +147,7 @@ const styles = StyleSheet.create({
   head: {
     letterSpacing: -1,
     fontWeight: 'bold',
+    color: COLORS.blue,
   },
   bottom: {
     width: '90%',
